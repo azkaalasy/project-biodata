9afca93f1f116ad991bc946a7d9cfc0f71bbd0c1
62878 <62878@LAPTOP-GQTIE1Q3>
2024-12-19 10:58:59 +0700

    instal commit

---
+++ src/main/java/com/mycompany/username/Username.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.username;
+
+/**
+ *
+ * @author 62878
+ */
+public class Username {
+    /**
+     * 
+     * @param args the command line arguments
+     */
+
+    public static void main(String[] args) {
+        katasandi lv=new katasandi();
+        lv.setLocationRelativeTo(null);
+        lv.setVisible(true);
+    }
+}
